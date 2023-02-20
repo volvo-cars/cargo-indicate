dd2e22fabff680b081891731b36c7df964447965
Emil Jonathan Eriksson <em5184er-s@student.lu.se>
2023-02-20 14:22:21 +0100

    First tests added

--- indicate/src/lib.rs
+++ indicate/src/lib.rs
@@ -32,12 +32,15 @@ type ObjectMap = BTreeMap<Arc<str>, FieldValue>;
 #[derive(Debug, Clone, Deserialize)]
 struct Query<'a> {
     pub query: &'a str,
-    pub variables: ObjectMap,
+    pub args: ObjectMap,
 }
 
 /// Executes a Trustfall query at a defined path, using the schema
 /// provided by `indicate`.
-pub fn execute_query(query_path: &Path, metadata_path: &Path) {
+pub fn execute_query(
+    query_path: &Path,
+    metadata_path: &Path,
+) -> Vec<BTreeMap<Arc<str>, FieldValue>> {
     let raw_query = fs::read_to_string(query_path)
         .expect("Could not read query at {path}!");
 
@@ -46,21 +49,16 @@ pub fn execute_query(query_path: &Path, metadata_path: &Path) {
 
     let metadata = extract_metadata_from_path(metadata_path);
     let adapter = Rc::new(RefCell::new(IndicateAdapter::new(&metadata)));
-    let res = trustfall_execute_query(
+    let res = match trustfall_execute_query(
         &SCHEMA,
         adapter,
         full_query.query,
-        full_query.variables,
-    );
-
-    match res {
-        Err(e) => panic!("Could not execute query due to error: {}", e),
-        Ok(i) => {
-            for r in i {
-                println!("{:#?}", r);
-            }
-        }
-    }
+        full_query.args,
+    ) {
+        Err(e) => panic!("Could not execute query due to error: {:#?}", e),
+        Ok(res) => res.collect(),
+    };
+    res
 }
 
 /// Extracts metadata from a `Cargo.toml` file by its direct path
@@ -75,23 +73,50 @@ pub fn extract_metadata_from_path(path: &Path) -> Metadata {
 
 #[cfg(test)]
 mod test {
-    use std::path::Path;
+    // use lazy_static::lazy_static;
+    use std::{fs, path::Path};
+    use test_case::test_case;
 
-    use crate::extract_metadata_from_path;
+    use crate::execute_query;
 
-    const TEST_ROOT: &'static str = "test_data/fake_crates";
+    // lazy_static! {
+    //     static ref TEST_ROOT: PathBuf = PathBuf::from("test_data/");
+    //     static ref TEST_CRATES: PathBuf =
+    //         PathBuf::from("test_data/fake_crates/");
+    //     static ref TEST_QUERIES: PathBuf = PathBuf::from("test_data/queries/");
+    // }
 
-    macro_rules! fake_crate {
-        ($name:literal) => {
-            Path::new(&format!("{TEST_ROOT}/{}/Cargo.toml", $name))
-        };
-    }
+    #[test_case("direct_dependencies", "direct_dependencies" ; "direct dependencies as listed in Cargo.toml")]
+    fn query_tests(fake_crate: &str, query_name: &str) {
+        let raw_cargo_toml_path =
+            format!("test_data/fake_crates/{fake_crate}/Cargo.toml");
+        let cargo_toml_path = Path::new(&raw_cargo_toml_path);
 
-    #[test]
-    #[ignore = "debugging purposes"]
-    fn dependency_resolve() {
-        let metadata =
-            extract_metadata_from_path(fake_crate!("direct_dependencies"));
-        println!("{:#?}", metadata.resolve.map(|n| n.nodes).unwrap());
+        let raw_query_path = format!("test_data/queries/{query_name}.in.ron");
+        let query_path = Path::new(&raw_query_path);
+
+        let raw_expected_result_path =
+            format!("test_data/queries/{query_name}.expected.json");
+        let expected_result_name = Path::new(&raw_expected_result_path);
+
+        let res = execute_query(query_path, cargo_toml_path);
+        let res_json_string = serde_json::to_string_pretty(&res)
+            .expect("Could not convert result to string");
+
+        let expected_result_string = fs::read_to_string(expected_result_name)
+            .unwrap_or_else(|_| {
+                panic!(
+                    "Could not read expected file '{}'",
+                    expected_result_name.to_string_lossy()
+                );
+            });
+
+        assert_eq!(
+            res_json_string.trim(),
+            expected_result_string.trim(),
+            "\nfailing query result:\n{}\n but expected:\n{}\n",
+            res_json_string,
+            expected_result_string
+        );
     }
 }

e77d7085b7cb9f45a587fe9977d2bf59ed6e7f66
Emil Jonathan Eriksson <em5184er-s@student.lu.se>
2023-02-22 16:32:16 +0100

    Add cache comment

--- indicate/src/github.rs
+++ indicate/src/github.rs
@@ -1,12 +1,16 @@
 //! Module providing connection to the GitHub API, with caching using ETags and
 //! the `httpcache` feature. With this feature, `304 Not Modified` responses
-//! from the GitHub will instead be fetched from a local cache
+//! from the GitHub will instead be fetched from a local cache.
 
 use lazy_static::lazy_static;
 use octorust::{auth::Credentials, http_cache::HttpCache, Client};
 
 lazy_static! {
     static ref GITHUB_CLIENT: octorust::Client = {
+        // It might be a good idea to cache GitHub URLs in a HashMap, that only
+        // exists in memory for one set of queries. This way, the amount of even
+        // slight requests is held at a minimum
+
         // TODO: This should probably be dynamic depending on settings and cfg
         let http_cache = <dyn HttpCache>::in_home_dir();
 

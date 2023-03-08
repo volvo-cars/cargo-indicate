df6aaf7109489499ed861a46a805c077528a85a3
Emil Jonathan Eriksson <em5184er-s@student.lu.se>
2023-03-08 15:59:34 +0100

    Make percentage_unsafe return values > 1 (percentage)

--- indicate/src/geiger.rs
+++ indicate/src/geiger.rs
@@ -31,14 +31,16 @@ impl GeigerCount {
         self.safe_count + self.unsafe_count
     }
 
-    /// Calculates what percentage of the code is `unsafe`
+    /// Calculates what percentage of the code is `unsafe`, rounded to two
+    /// decimal points
     ///
     /// Since `total_count` >= `unsafe_count`, this function will handle
     /// `0 / 0` to be equal to `0.0` (all code is safe, there is no code).
     pub fn percentage_unsafe(&self) -> f64 {
         let res = f64::from(self.unsafe_count) / f64::from(self.total_count());
         if res.is_finite() {
-            res
+            // We only really care about at most two decimal points
+            (res * 10000.0).round() / 100.0
         } else {
             0.0
         }
@@ -51,8 +53,9 @@ mod test {
     use test_case::test_case;
 
     #[test_case(0, 0 => 0.0)]
-    #[test_case(3, 1 => 0.25)]
-    #[test_case(9, 1 => 0.1)]
+    #[test_case(3, 1 => 25.0)]
+    #[test_case(9, 1 => 10.0)]
+    #[test_case(2, 1 => 33.33)]
     fn percentage_unsafe(safe_count: u32, unsafe_count: u32) -> f64 {
         GeigerCount::new(safe_count, unsafe_count).percentage_unsafe()
     }

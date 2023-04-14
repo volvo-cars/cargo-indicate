8194b1d37874e232e64608c4adbfd3d4b02fdb53
Emil Jonathan Eriksson <em5184er-s@student.lu.se>
2023-04-14 11:54:47 +0200

    Add framework for CratesIoClient

--- indicate/src/crates_io.rs
+++ indicate/src/crates_io.rs
@@ -1,13 +1,24 @@
+//! Client and methods for retrieving information from the crates.io API
+//!
+//! _Note_: Due to the crates.io crawler policy, the amount of requests that
+//! can be made is limited. [`CratesIoClient`] attempts to make this less
+//! noticeable with caching and doing large fetches, but please keep this in
+//! mind.
+//! 
+//! See https://crates.io/policies#crawlers for more information.
+
 use std::{collections::HashMap, time::Duration};
 
-use crates_io_api::{SyncClient, FullCrate};
+use crates_io_api::{SyncClient, FullCrate, FullVersion};
+
+use crate::NameVersion;
 
 /// Wrapper around a [`crates_io_api::SyncClient`], with added caching
 pub struct CratesIoClient {
     client: SyncClient,
 
     /// Cache between crate name and information about it
-    cache: HashMap<String, FullCrate>
+    cache: HashMap<String, FullCrate>,
 }
 
 impl CratesIoClient {
@@ -21,11 +32,34 @@ impl CratesIoClient {
             cache: HashMap::new(),
         }
     }
+
+    pub fn full_crate(crate_name: &str) -> &FullCrate {
+        todo!()
+    }
+
+    pub fn full_version(name_version: &NameVersion) -> &FullVersion {
+        todo!()
+    }
+
+    /// Retrieves the total amount of downloads for a crate, all versions
+    ///
+    /// # See also
+    /// [`version_downloads`](CratesIoClient::version_downloads)
+    pub fn total_downloads(crate_name: &str) -> u64 {
+        todo!()
+    }
+
+    /// Retrieves the total amount of downloads for a specific crate version
+    ///
+    /// # See also
+    /// [`total_downloads`](CratesIoClient::total_downloads)
+    pub fn version_downloads(name_version: &NameVersion) -> u64 {
+        todo!()
+    }
 }
 
 impl Default for CratesIoClient {
     fn default() -> Self {
-        // See https://crates.io/policies#crawlers
         let user_agent = std::env::var("USER_AGENT")
             .expect("USER_AGENT environment variable not set");
         Self::new(&user_agent, Duration::from_secs(1))

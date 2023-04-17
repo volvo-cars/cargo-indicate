a9b861d78498c88c00df4395881e816ab88681c5
Emil Jonathan Eriksson <em5184er-s@student.lu.se>
2023-04-17 12:45:19 +0200

    Improve crates io performance a lot

--- indicate/src/crates_io.rs
+++ indicate/src/crates_io.rs
@@ -7,10 +7,9 @@
 //! 
 //! See https://crates.io/policies#crawlers for more information.
 
-use std::{collections::HashMap, time::Duration, rc::Rc};
+use std::{collections::HashMap, time::Duration};
 
-use crates_io_api::{SyncClient, FullCrate};
-use rustsec::Version;
+use crates_io_api::{SyncClient, Crate, CrateResponse, Version};
 
 use crate::NameVersion;
 
@@ -18,8 +17,12 @@ use crate::NameVersion;
 pub struct CratesIoClient {
     client: SyncClient,
 
-    /// Cache between crate name and information about it
-    cache: HashMap<String, Rc<FullCrate>>,
+    /// Cache between crate name and downloads info
+    ///
+    /// We do not want requests for the same crate to fail and then work in the
+    /// same query, so we store if we were able to find it the first time via
+    /// the option.
+    cache: HashMap<String, Option<CrateResponse>>,
 }
 
 impl CratesIoClient {
@@ -34,44 +37,50 @@ impl CratesIoClient {
         }
     }
 
-    pub fn full_crate(&mut self, crate_name: &str) -> Option<Rc<FullCrate>> {
-        // TODO: Remove this Rc
-        match self.cache.get(crate_name) {
-            Some(fc) => Some(Rc::clone(fc)),
-            None => {
-                match self.client.full_crate(crate_name, true) {
-                    Ok(fc) => {
-                        self.cache.insert(crate_name.to_string(), Rc::new(fc));
-                        // Avoid borrow after move
-                        Some(Rc::clone(self.cache.get(crate_name).unwrap()))
-                    }
-                    Err(e) => {
-                        eprintln!("failed to retrieve crates.io information about {crate_name} due to error: {e}");
-                        None
-                    }
+    /// Retrieves information about a crate from the `crates.io` API
+    ///
+    /// Will return `None` if the request fails, and will cache this crate as
+    /// such.
+    pub fn crate_response(&mut self, crate_name: &str) -> Option<&mut CrateResponse> {
+        self.cache.entry(crate_name.to_string()).or_insert_with(|| {
+           match self.client.get_crate(crate_name)  {
+                Ok(cr) => Some(cr),
+                Err(e) => {
+                    eprintln!("failed to retrieve crates.io information about {crate_name} due to error: {e}");
+                    None
                 }
             }
-        }
+        }).as_mut()
+       }
+
+    /// Retrieve data about a crate from the `crates.io` API
+    pub fn crate_data(&mut self, crate_name: &str) -> Option<&Crate> {
+        self.crate_response(crate_name).map(|cr| &cr.crate_data)
+    }
+
+    /// Retrieves data about all versions of a crate from the `crates.io` API
+    pub fn versions(&mut self, crate_name: &str) -> Option<&Vec<Version>> {
+        self.crate_response(crate_name).map(|cr| &cr.versions)
     }
 
     /// Retrieves the total amount of downloads for a crate, all versions
     pub fn total_downloads(&mut self, crate_name: &str) -> Option<u64> {
-        self.full_crate(crate_name).map(|fc| fc.total_downloads)
+        self.crate_data(crate_name).map(|c| c.downloads)
     }
 
     /// Retrieves the total amount of downloads for a crate, all versions
     pub fn recent_downloads(&mut self, crate_name: &str) -> Option<u64> {
-        self.full_crate(crate_name).and_then(|fc| fc.recent_downloads)
+        self.crate_data(crate_name).and_then(|c| c.recent_downloads)
     }
 
     /// Retrieves the total amount of downloads for a specific crate version
     pub fn version_downloads(&mut self, name_version: &NameVersion) -> Option<u64> {
-        self.full_crate(&name_version.name).and_then(|fc| {
-            fc.versions.iter().find_map(|fv| {
-                match Version::parse(&fv.num) {
+        self.versions(&name_version.name).and_then(|versions| {
+            versions.iter().find_map(|v| {
+            match rustsec::Version::parse(&v.num) {
                     Ok(current_version) => {
                         if current_version == name_version.version {
-                            Some(fv.downloads)
+                            Some(v.downloads)
                         } else {
                             None
                         }
@@ -80,19 +89,17 @@ impl CratesIoClient {
                         eprintln!("could not parse crates.io version for {name_version:?} due to error: {e}");
                         None
                     }
-                }
-            })
-        })
+                }        })        })
     }
 
     /// Returns if this version is yanked from `crates.io`
     pub fn yanked(&mut self, name_version: &NameVersion) -> Option<bool> {
-        self.full_crate(&name_version.name).and_then(|fc| {
-            fc.versions.iter().find_map(|fv| {
-                match Version::parse(&fv.num) {
+        self.versions(&name_version.name).and_then(|versions| {
+            versions.iter().find_map(|v| {
+                match rustsec::Version::parse(&v.num) {
                     Ok(current_version) => {
                         if current_version == name_version.version {
-                            Some(fv.yanked)
+                            Some(v.yanked)
                         } else {
                             None
                         }
@@ -111,8 +118,8 @@ impl CratesIoClient {
     /// If only the count of yanked versions is desired, use
     /// [`yanked_versions_count`](Self::yanked_versions_count) instead.
     pub fn yanked_versions(&mut self, crate_name: &str) -> Option<Vec<String>> {
-        self.full_crate(crate_name).map(|fc| {
-            fc.versions.iter().filter_map(|fv| {
+        self.versions(crate_name).map(|versions| {
+            versions.iter().filter_map(|fv| {
                 if fv.yanked {
                     // We do not parse version, as that may fail, leading
                     // to odd results
@@ -128,8 +135,8 @@ impl CratesIoClient {
     /// crate
     pub fn yanked_versions_count(&mut self, crate_name: &str) -> Option<usize> {
         // Do not rely on Self::yanked_version, as it is more expensive
-        self.full_crate(crate_name).map(|fc| {
-            fc.versions.iter().filter(|fv| fv.yanked).count()
+        self.versions(crate_name).map(|versions| {
+            versions.iter().filter(|v| v.yanked).count()
         })
     }
 }
